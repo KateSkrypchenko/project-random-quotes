7c1219489e76d177e5b04a542059a9a0edd8420c
user <katalonij440@gmail.com>
2022-12-09 16:48:12 +0200

    corrected

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 const axios = require('axios').default;
 
 const refs = {
-  button: document.querySelector('.btn'),
+  body: document.querySelector('body'),
   quote: document.querySelector('.quote'),
   author: document.querySelector('.author'),
+  btnStart: document.querySelector('.btn-start'),
+  btnStop: document.querySelector('.btn-stop'),
 };
 
 const options = {
@@ -16,17 +18,33 @@ const options = {
   },
 };
 
-refs.button.addEventListener('click', onClickButton);
+refs.btnStop.disabled = true;
+const TIME_CHANGE = 3000;
+let timerId = null;
 
-function onClickButton() {
-  console.log('dfjsdf');
-  createQuotes();
+refs.btnStart.addEventListener('click', onClickButtonStart);
+refs.btnStop.addEventListener('click', onClickButtonStop);
+
+function onClickButtonStart() {
+  addRemoveClass();
+  toggleDisableButton(true, false);
+  timerId = setInterval(onStartChange, TIME_CHANGE);
+}
+
+function onClickButtonStop() {
+  addRemoveClass();
+  toggleDisableButton(false, true);
+  clearInterval(timerId);
+}
+
+async function onStartChange() {
+  await createQuotes();
+  await renderOfGradient();
 }
 
 async function createQuotes() {
   try {
     const fetch = await getQuotes();
-    console.log(fetch);
     refs.author.textContent = `- ${fetch.author}`;
     refs.quote.textContent = `"${fetch.body}"`;
   } catch (error) {
@@ -42,4 +60,41 @@ async function getQuotes() {
   } catch (error) {
     throw new Error(error);
   }
+}
+
+// button
+
+function toggleDisableButton(start, stop) {
+  refs.btnStart.disabled = start;
+  refs.btnStop.disabled = stop;
+}
+
+// color
+
+function renderOfGradient() {
+  refs.body.style.background = `linear-gradient(${random(0, 360)}deg, ${changeColor()})`;
+}
+
+function changeColor() {
+  const amountColor = random(2, 4);
+  const arrOfColor = [];
+  for (let i = 0; i < amountColor; i += 1) {
+    arrOfColor[i] = getRandomHexColor();
+  }
+  return arrOfColor;
+}
+
+function random(min, max) {
+  return Math.round(min + Math.random() * (max - min));
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// animation
+
+function addRemoveClass() {
+  refs.quote.classList.toggle('animation');
+  refs.author.classList.toggle('animation');
 }
